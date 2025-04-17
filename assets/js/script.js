33bbb5e078c8744143dc6796c8109a637bdc20e0
Oscar <ocariaga2024@alu.uct.cl>
2025-04-17 16:46:25 -0400

    Se eliminaron archivos extras, se añadieron comentarios

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,15 +3,15 @@ function AñadirOrden()
     // Función mock
     event.preventDefault()
     alert("Preorden exitosa")
-    window.location.href = "/index.html";
+    window.location.href = "/index.html"; // Evita que el formulario redirija a la página con un query string
 }
 
 function VentanaPreorden()
 {
-    document.getElementById("flotante-contenedor").style.display = "flex"
+    document.getElementById("flotante-contenedor").style.display = "flex" // Mostrar ventana de formulario
 }
 
 function CerrarVentanaPreorden()
 {
-    document.getElementById("flotante-contenedor").style.display = "none"
+    document.getElementById("flotante-contenedor").style.display = "none" // Ocultar ventana de formulario
 }
